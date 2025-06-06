3874f43d71caf6f913a1353d00112ef69d3e9278
Sebastian Korfmann <sebastian@korfmann.net>
2025-06-06 08:47:49 +0200

    Initial commit: Hello World React app

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World!</h1>
+      <p>Welcome to my React app deployed on Vercel</p>
+    </div>
+  )
+}
+
+export default App
